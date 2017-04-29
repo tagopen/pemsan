afa72683965e1afa0d7ffb8918508a4876437ad1
marchik88 <marchik88@rambler.ru>
2017-04-29 18:19:17 +0300

    product modal fix size

--- app/mail/mail.php
+++ app/mail/mail.php
@@ -8,6 +8,7 @@
   }
 
   $http_host = $_SERVER['HTTP_HOST'];
+  $body = '';
 
   if ( substr($http_host, 0, 4)=='www.') {
     $host_name = substr($http_host, 4);
@@ -23,76 +24,47 @@
     'host_dir'      => HTTP_SERVER,
     );
 
-  if (!empty($_POST["user_form"])) {
-    $post['user_form'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+  if (!empty($_POST["form"])) {
+    $post['user_form'] = filter_input(INPUT_POST, 'form', FILTER_SANITIZE_EMAIL);
+    $body .= 'Заявка с формы: ' . $post['user_form'] . chr(10) . chr(13);
   }
 
   if (!empty($_POST["email"])) {
     $post['user_email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $body .= 'Почта: ' . $post['user_email'] . chr(10) . chr(13);
   }
 
   if (!empty($_POST["name"])) {
     $post['user_name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $body .= 'Имя: ' . $post['user_name'] . chr(10) . chr(13);
   }
 
   if (!empty($_POST["phone"])) {
     $post['user_phone'] = filter_input(INPUT_POST,'phone', FILTER_SANITIZE_STRING);
+    $body .= 'Телефон: ' . $post['user_phone'] . chr(10) . chr(13);
   }
 
   if (!empty($_POST["message"])) {
-    $post['user_message'] = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+    $post['user_message'] = filter_input(INPUT_POST,'message', FILTER_SANITIZE_STRING);
+    $body .= 'Сообщение: ' . $post['user_message'] . chr(10) . chr(13);
   }
 
-  // Insert form data into html
-  $patterns = array();
-  $replacements = array();
-  foreach ($post as $key => $value) {
-    $patterns[] = '/\{%' . $key .  '\%}/i'; // varible => {$varible}
-    $replacements[] = $value;
-  }
 
-  // html template
-  $body = '';
-  if (is_file('html_template.html')) {
-    $html_template = file_get_contents('html_template.html');
-    $body = preg_replace($patterns, $replacements, $html_template);  
-    $body = preg_replace('/\{%(.+)%\}/', '', $body); // remove all beetween "{% %}"
-  }
-
-  // If mailer not supported html
-  $altBody = '';
-  if (is_file('no_html.html')) {
-    $no_html_template = file_get_contents('no_html.html');
-    $altBody = preg_replace($patterns, $replacements, $no_html_template);
-    $altBody = preg_replace('/\{%(.+)%\}/', '', $altBody); // remove all beetween "{% %}"
-  }
   $mail = new PHPMailer();
 
   $mail->CharSet      = 'UTF-8';
 
-  //if mail is SMTP
-  /*
-  $mail->isSMTP();
-  $mail->Host         = 'smtp.server.com';
-  $mail->SMTPAuth     = true;
-  $mail->SMTPSecure   = 'ssl';
-  $mail->Port         = 465;
-  $mail->Username     = 'name@mail.com';
-  $mail->Password     = 'password';
-  */
-
   $mail->IsSendmail();
 
-  $from = 'no-reply@tagopen.com';
+  $from = 'no-repeat@tagopen.com';
   $to = "marchik88@rambler.ru";
   $mail->SetFrom($from, HOST_NAME);
-  $mail->AddAddress($to, 'Name Surname');
+  $mail->AddAddress($to);
 
   $mail->isHTML(false);
 
   $mail->Subject      = "Новая заявка с сайта";
   $mail->Body         = $body;
-  $mail->AltBody      = $altBody;
 
   if(!$mail->send()) {
     echo 'Что-то пошло не так. ' . $mail->ErrorInfo;
